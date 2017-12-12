714a7cf578bce4cc2ec1617b9b44c42480e25530
SamirZ <z.samir@live.com>
2017-12-12 16:27:35 +0100

    Eddited about page

---
+++ src/main/resources/static/js/devopsbuddy.js
@@ -0,0 +1,10 @@
+$(function main() {
+    $('.btn-collapse').click(function (e) {
+        e.preventDefault();
+        var $this = $(this);
+        var $collapse = $this.closest('.collapse-group').find('.collapse');
+        $collapse.collapse('toggle');
+    });
+});
+
+
